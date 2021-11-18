990817c26017e3ef1731dfda391f3d7e2399fd63
Daryl Hewett <dazza1308+github@gmail.com>
2021-11-18 19:08:58 +0000

    More eslint/ts configs

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,12 @@
 "use strict";
-const express = require('express');
-const app = express();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    res.send('huehuehuehuehueheu');
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-const express = require('express')
+import express, { Application } from 'express'
 
-const app = express()
+const app: Application = express()
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('huehuehuehuehueheu')
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log('Server running on port ' + PORT))
