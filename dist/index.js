52c4f79e8515877b6e3911515dd0a7315f5cd37c
Daryl Hewett <dazza1308+github@gmail.com>
2021-11-18 18:24:17 +0000

    Basis eslint and TS setup

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Server running on port ' + PORT));
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-const express = require("express");
-const app = express();
+const express = require('express')
 
-console.log("Hello Wolrd"!);
+const app = express()
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log('Server running on port ' + PORT))
