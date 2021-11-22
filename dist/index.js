95b8bbc45b78f7a31d358d522479d569a530ccab
Daryl Hewett <daryl@corbyfellas.com>
2021-11-22 14:14:27 +0000

    added dist to gitignore

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const db_connect_1 = __importDefault(require("./utils/db.connect"));
 const app = (0, express_1.default)();
 app.get('/', (req, res) => {
     res.send('huehuehuehuehueheu');
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
+(0, db_connect_1.default)();
